c27f8516ee3e9146916136f2f9c573e8dcf797c1
itKhaiNDdotDev <khai12345678t@gmail.com>
2023-04-27 18:19:10 +0700

    Feature: Impliment vuetify layouts.

---
+++ Graduation.BrainwaveSystem.APIs/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Graduation.BrainwaveSystem.Models.Entities;
+
+namespace Graduation.BrainwaveSystem.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Graduation.BrainwaveSystem.Models.Entities.Device> Device { get; set; } = default!;
+
+        public DbSet<Graduation.BrainwaveSystem.Models.Entities.DeviceData>? DeviceData { get; set; }
+
+        public DbSet<Graduation.BrainwaveSystem.Models.Entities.DataRawEEG>? DataRawEEG { get; set; }
+
+        public DbSet<Graduation.BrainwaveSystem.Models.Entities.Data8BandsEEG>? Data8BandsEEG { get; set; }
+    }
+}
--- Graduation.BrainwaveSystem.Apps.Web/src/App.vue
+++ Graduation.BrainwaveSystem.Apps.Web/src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-layout>
+       <!-- <v-system-bar color="grey-darken-3"></v-system-bar> -->
+
+      <!-- <v-navigation-drawer
+        color="grey-darken-3"
+        permanent
+        width="256"
+      ></v-navigation-drawer> -->
+      <VtfSideBar/>
+
+      <!-- <v-app-bar
+        color="grey"
+        height="64"
+        flat
+      ></v-app-bar> -->
+      <VtfAppBar/>
+
+      <!-- <v-navigation-drawer
+        color="grey-lighten-1"
+        location="right"
+        permanent
+        width="150"
+      ></v-navigation-drawer> -->
+
+      <!-- <v-app-bar
+        color="grey-lighten-2"
+        flat
+        height="48"
+        location="bottom"
+      ></v-app-bar> -->
+
+      <!-- <VtfFooter /> -->
+
+      <v-main>
+        <VtfContent/>
+      </v-main>
+    </v-layout>
+    <VtfFooter/>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VtfAppBar from './layouts/VtfAppBar.vue';
+import VtfContent from './layouts/VtfContent.vue';
+import VtfSideBar from './layouts/VtfSideBar.vue';
+import VtfFooter from "./layouts/VtfFooter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    VtfFooter,
+    VtfAppBar,
+    VtfSideBar,
+    VtfContent
+  },
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
