4819628d342a2146c82ed577a1ef1b38fb9cf627
itKhaiNDdotDev <khai12345678t@gmail.com>
2023-05-04 10:04:31 +0700

    Feature: Fill Line Chart on Vuetify.

---
+++ Graduation.BrainwaveSystem.Apps.Web/src/components/KAreaChart.vue
@@ -0,0 +1,70 @@
+<template>
+  <Line :options="chartOptions" :data="chartData" />
+</template>
+
+<script>
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "KAreaChart",
+  components: { Line },
+
+  props: {
+    labelProps: Array,
+    dataProps: Array
+  },
+
+  data() {
+    return {
+      chartData: {
+        labels: this.labelProps,//["January", "February", "March", "April", "May", "Jun", "July"],
+        datasets: [
+            {
+                label: "One",
+                data: this.dataProps[0],//[40, 20, 12, 3, 4, 5, 1] ,
+                fill: true,
+                tension: 0.25,
+                backgroundColor: "#FF002244"
+            },
+            {
+                label: "Two",
+                data: this.dataProps[1],//[15, 22, 12, 13, 24, 5, 41] ,
+                fill: true,
+                tension: 0.25,
+                backgroundColor: "#00FF8844"
+            }
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+
+  onUpdated() {
+    console.log(this.chartData.labels);
+  },
+};
+</script>
--- Graduation.BrainwaveSystem.Apps.Web/src/layouts/VtfContent.vue
+++ Graduation.BrainwaveSystem.Apps.Web/src/layouts/VtfContent.vue
@@ -1,21 +1,13 @@
 <template>
- <v-card>
-    <v-tabs
-      v-model="tab"
-      color="deep-purple-accent-4"
-      align-tabs="center"
-    >
-      <v-tab :value="1">Landscape</v-tab>
-      <v-tab :value="2">City</v-tab>
-      <v-tab :value="3">Abstract</v-tab>
+  <v-card>
+    <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="center">
+      <v-tab :value="1">General</v-tab>
+      <v-tab :value="2">TMGA Results</v-tab>
+      <v-tab :value="3">Raw Data</v-tab>
     </v-tabs>
     <v-window v-model="tab">
-      <v-window-item
-        v-for="n in 3"
-        :key="n"
-        :value="n"
-      >
-        <v-container fluid>
+      <v-window-item v-for="n in 3" :key="n" :value="n">
+        <!-- <v-container fluid>
           <v-row>
             <v-col
               v-for="i in 6"
@@ -30,19 +22,68 @@
               ></v-img>
             </v-col>
           </v-row>
-        </v-container>
+        </v-container> -->
+        <!-- <v-sheet
+          class="v-sheet--offset mx-auto"
+          color="cyan"
+          elevation="12"
+          max-width="calc(100% - 32px)"
+        >
+          <v-sparkline
+            :labels="labels"
+            :value="values"
+            color="white"
+            line-width="2"
+            padding="16"
+          ></v-sparkline>
+        </v-sheet> -->
+        <KAreaChart v-if="!IsProcessing" :labelProps="labels" :dataProps="values"/>
+        <div v-else>Loading</div>
       </v-window-item>
+      <input type="text" v-bind="inputLabel">
+      <input type="text" v-bind="inputVal1">
+      <input type="text" v-bind="inputVal2">
+      <button @click="onClickPush">Push</button>
     </v-window>
   </v-card>
 </template>
 <script>
+import KAreaChart from '@/components/KAreaChart.vue';
 //import { md2 } from 'vuetify/blueprints'
 
 export default {
-    data: () => ({
+  components: {
+    KAreaChart
+  },
+
+  data() {
+    return {
       tab: null,
-    }),
-  }
+    labels: ["12am", "3am", "6am", "9am", "12pm", "3pm", "6pm", "9pm"],
+    values:[ [200, 675, 410, 390, 310, 460, 250, 240], [410, 390, 310, 460, 120, 130, 520, 400] ],
+    inputLabel: "",
+    inputVal1: "",
+    inputVal2: "",
+    IsProcessing: false
+    }
+  },
+
+  methods: {
+    pushData(newLabel, val1, val2) {
+      console.log(val1 + val2)
+      this.IsProcessing = true;
+      this.labels.push(newLabel);
+      //this.values[0].push(val1);
+      //this.values[1].push(val2);
+      this.IsProcessing = false;
+    },
+
+    onClickPush() {
+      console.log("IN EVENT")
+      this.pushData(this.inputLabel, parseInt(this.inputVal1), parseInt(this.inputVal2));
+    }
+  },
+};
 </script>
 
 <style lang="">
