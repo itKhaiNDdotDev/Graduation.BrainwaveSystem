312fc2e86b59e989ffe52592d1f917e83c44164a
Nguyen Duy Khai <53107020+itKhaiNDdotDev@users.noreply.github.com>
2023-07-10 11:38:13 +0700

    Config: Use Dev Exception Page for all enviroment to test deployment.

--- Graduation.BrainwaveSystem.APIs/Program.cs
+++ Graduation.BrainwaveSystem.APIs/Program.cs
@@ -45,6 +45,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
+    app.UseDeveloperExceptionPage();  
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
